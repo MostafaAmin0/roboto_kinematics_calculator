be0c1da7bc1ee8e1ff3a76079c0fe0b0c5b00c0d
MostafaAmin0 <mostafa.aminmk@gmail.com>
2022-03-25 19:23:41 +0200

    initial file

---
+++ robot.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+#--------------------------------------------------------
+#set the manipulator configurations
+
+#DOF & type of joints
+def setJoints ():
+    pass
+
+# DH Parameter
+def setDHParameter():
+    pass
+
+#------------------------------------------------------
+#helper functions to return the A matrices
+#A1
+def matrix_A():
+    pass
+
+# A1----An
+def list_A_matrix():
+    pass
+
+#------------------------------------------------------
+#helper functions to return the Transformatiom matrices
+#T0
+def transformationMatrix ():
+    pass
+
+# T0----Tn
+def list_TF_matrix():
+    pass
+
+#-----------------------------------------------------
+#driver function to implement the forward kienmatics
+
+def fK ():
+    pass
+
+#-----------------------------------------------------
+#driver function to implement jacobian matrix
+
+def getJacobian():
+    pass
+
+#-----------------------------------------------------
+#driver function to implement the inverse kienmatics
+def iK():
+    pass
+
+#------------------------------------------------------
+#driver function to implement the path planning 
+
+def path_planning():
+    pass
+
+#-------------------------------------------------------
+# start of main code
+# call your functions here and take input from user 
+
+print('alo')
+
